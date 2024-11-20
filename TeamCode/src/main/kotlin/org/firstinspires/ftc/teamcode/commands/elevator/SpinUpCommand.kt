f127366f40f7cbda4ffb5608f266df63ae21d27f
Sachet Korada <sachet.korada@gmail.com>
2024-11-20 12:10:42 -0500

    Cleanup

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/elevator/SpinUpCommand.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/elevator/SpinUpCommand.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/slides/SpinUpCommand.kt
-package org.firstinspires.ftc.teamcode.commands.slides
-========
 package org.firstinspires.ftc.teamcode.commands.elevator
->>>>>>>> 509c48b (Arm PID infra:):TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/elevator/SpinUpCommand.kt
 
 import com.arcrobotics.ftclib.command.CommandBase
 import org.firstinspires.ftc.teamcode.subsystems.slides.OpenSlidesSubsystem
