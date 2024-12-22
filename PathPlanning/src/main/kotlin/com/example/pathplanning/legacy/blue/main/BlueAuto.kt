d8ab9a73c40e2d5c8bf04b73dc5da026f774823a
AdityaP241 <adi24.patil@gmail.com>
2024-12-22 13:10:13 -0500

    1+1 park

--- PathPlanning/src/main/kotlin/com/example/pathplanning/legacy/blue/main/BlueAuto.kt
+++ PathPlanning/src/main/kotlin/com/example/pathplanning/legacy/blue/main/BlueAuto.kt
@@ -1,108 +1,37 @@
-package com.example.pathplanning.legacy.blue.main
+package com.example.meepmeeptesting
 
-//object BlueAuto {
-//    @JvmStatic
-//
-//    fun main(args: Array<String>) {
-//        val meepMeep = MeepMeep(800)
-//
-//        val myBot =
-//            DefaultBotBuilder(meepMeep) // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-//                .setConstraints(30.0, 30.0, Math.toRadians(170.0), Math.toRadians(170.0), 13.94)
-//                .followTrajectorySequence { drive ->
-//                    drive.trajectorySequenceBuilder(Pose2d(-12.0, 62.0, (270.0).toRadians()))
-//                        .addTemporalMarker(0.1,) {
-//                            //add the command for the arm to go up
-//                            //add command for belt servo to go backwards
-//                            //add the command to add the slides go up
-//                        }
-//                        .splineToLinearHeading(Pose2d(55.0, 55.0, Math.toRadians(225.0)), Math.toRadians(0.0))
-//                        .addDisplacementMarker {
-//                            //add command for rollers servo to outtake
-//                        }
-//                        .turn(Math.toRadians(30.0)) //
-//                        .addTemporalMarker(6.0) {
-//                            //add command for arm to go down
-//                            //add command for slides to go out
-//                            //add command to belt servo to go at [x] angle
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to intake
-//                        }
-//                        .turn(Math.toRadians(-30.0)) //
-//                        .addTemporalMarker() {
-//                            //add command to bring slides in
-//                            //add command to bring arm up
-//                            //add command to put slides out
-//                            //add command to turn belt servo
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to outtake
-//                        }
-//                        .turn(Math.toRadians(55.0)) //
-//                        .addTemporalMarker() {
-//                            //add command to bring slides in
-//                            //add command to bring arm down
-//                            //add command to put slides out
-//                            //add command to turn belt servo
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to intake
-//                        }
-//                        .turn(Math.toRadians(-55.0)) //
-//                        .addTemporalMarker() {
-//                            //add command to bring slides in
-//                            //add command to bring arm up
-//                            //add command to put slides out
-//                            //add command to turn belt servo
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to outtake
-//                        }
-//                        .turn(Math.toRadians(80.0)) //
-//                        .addTemporalMarker() {
-//                            //add command to bring slides in
-//                            //add command to bring arm down
-//                            //add command to put slides out
-//                            //add command to turn belt servo
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to intake
-//                        }
-//                        .turn(Math.toRadians(-80.0)) //
-//                        .addTemporalMarker() {
-//                            //add command to bring slides in
-//                            //add command to bring arm up
-//                            //add command to put slides out
-//                            //add command to turn belt servo
-//                        }
-//                        .addDisplacementMarker {
-//                            //add command to outtake
-//                        }
-//                        .splineToSplineHeading(Pose2d(36.0, 12.0, Math.toRadians(-180.0)), Math.toRadians(-90.0))
-//                        .addDisplacementMarker {
-//                            //add command to bring arm up
-//                            //add command to bring slides out
-//                        }
-//
-//
-//
-//
-//                        .build()
-//                }
-//        var img: Image? = null
-//        try {
-//            img = ImageIO.read(File("C://Users//arava//Downloads//field.png/"))
-//        } catch (_ : IOException)  {
-//
-//        }
-//
-//        if (img != null) {
-//            meepMeep.setBackground(img)
-//                .setDarkMode(true)
-//                .setBackgroundAlpha(0.95f)
-//                .addEntity(myBot)
-//                .start()
-//        }
-//    }
-//}
+import com.acmerobotics.roadrunner.Pose2d
+import com.noahbres.meepmeep.MeepMeep
+import com.noahbres.meepmeep.MeepMeep.Background
+import com.noahbres.meepmeep.core.toDegrees
+import com.noahbres.meepmeep.core.toRadians
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
+
+object MeepMeepTesting {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val meepMeep = MeepMeep(800)
+
+        val myBot =
+            DefaultBotBuilder(meepMeep) // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(30.0, 30.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
+                .build()
+
+        myBot.runAction(
+            myBot.drive.actionBuilder(Pose2d(14.0, -62.0, 90.0.toRadians()))
+
+                .splineToLinearHeading(Pose2d(0.0,-34.0,90.0.toRadians()),90.0.toRadians())
+                .waitSeconds(0.1)
+                .splineToLinearHeading(Pose2d(16.0,-60.0,0.0.toRadians()),0.0.toRadians())
+                .splineToLinearHeading(Pose2d(34.0,0.0,0.0.toRadians()),90.0.toRadians())
+                .splineToLinearHeading(Pose2d(24.0,0.0,0.0.toRadians()),90.0.toRadians())
+                .build()
+        )
+
+        meepMeep.setBackground(Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start()
+    }
+}
